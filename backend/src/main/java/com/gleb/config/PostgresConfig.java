b8756fb6e8c8de59d49a73aab6c1cd6e9d33defc
gleb12FW2 <glscherbina03@ukr.net>
2023-06-12 22:08:58 +0300

    (feat) Configured r2dbc connection to PostgreSQL

---
+++ backend/src/main/java/com/gleb/config/PostgresConfig.java
@@ -0,0 +1,25 @@
+package com.gleb.config;
+import io.r2dbc.spi.Connection;
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import org.reactivestreams.Publisher;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+@Configuration
+
+public class PostgresConfig  {
+
+    ConnectionFactory connectionFactory = ConnectionFactories.get("r2dbc:postgresql://localhost:5432/userDb");
+    Publisher<? extends Connection> connectionPublisher = connectionFactory.create();
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+
+        return initializer;
+    }
+}
