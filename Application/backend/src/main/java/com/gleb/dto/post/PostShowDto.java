9f6bd05cfc044b18af81f17c10df807fb43015bd
gleb12FW2 <glscherbina03@ukr.net>
2023-07-28 13:55:17 +0300

    feat(web , security): implemented api of getting and deleting published posts of other users by moderator

---
+++ Application/backend/src/main/java/com/gleb/dto/post/PostShowDto.java
@@ -0,0 +1,18 @@
+package com.gleb.dto.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostShowDto {
+    private String  title;
+    private String  content;
+    private String  authorName;
+    private LocalDateTime publishedDate;
+}
